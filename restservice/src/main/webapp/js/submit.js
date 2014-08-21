aa4f40f7c17e13ff737d44bc18db9e7ec4cbc10e
Deeksha Sharma <deeksha.sharma25@gmail.com>
2014-08-21 13:41:54 -0700

    submit.js is update but tweets not rendered on web page. Also there is a rate limiting issue with twitter

--- restservice/src/main/webapp/js/submit.js
+++ restservice/src/main/webapp/js/submit.js
@@ -1,4 +1,5 @@
-function submitForm()
+
+function clickAlchemy()
 {
     var searchVal = $("#searchTerm").val();
     if (searchVal.length  < 1) {
@@ -8,14 +9,17 @@ function submitForm()
 
     var getUrl = "http://localhost:8080/restservice-1.0/rest/twitter/query?keyword=" + searchVal + "&algorithm=A";
     console.log("getting data from:", getUrl);
+    $.get(getUrl, function( data )
+    {
+        var result = JSON.stringify(data);
+        renderResults();
 
-    $.get( getUrl, function( data ) {
-//      $( ".result" ).html( data );
-      console.log("received:" + JSON.stringify(data));
+      $( ".result" ).html( data );
+//      console.log("received:" + JSON.stringify(data));
     });
 }
 
-function nlpAlgorithm()
+function clickNLP()
 {
     var searchVal = $("#searchTerm").val();
     if (searchVal.length  < 1) {
@@ -23,16 +27,24 @@ function nlpAlgorithm()
         return;
     }
 
-//    var getUrl = "http://localhost:8080/restservice-1.0/rest/twitter/query?keyword=" + searchVal + "&algorithm=B";
-//    console.log("getting data from:", getUrl);
+    var getUrl = "http://localhost:8080/restservice-1.0/rest/twitter/query?keyword=" + searchVal + "&algorithm=N";
+    console.log("getting data from:", getUrl);
 
-//    $.get( getUrl, function( data ) {
+    $.get( getUrl, function( data )
+    {
+        String results = JSON.stringify(data);
 //      console.log("received:" + JSON.stringify(data));
       renderResults();
-//    });
+    });
 }
 
-var results = [{"line":"Police firing tear gas at protesters?\n\n@BarackObama, WHAT KIND OF SHIT HOLE ARE YOU RUNNING HERE?! #Ferguson","sentiment":"Negative"},{"line":"barackobama: sirlightbulb: dear god I hope that no one on this website ever tries to run for president... http://t.co/a5G8AfX8bA","sentiment":"Neutral"},{"line":"@BarackObama @GovJayNixon what's happening in #Ferguson tonight is disgraceful.  The world is watching this.  #PoliceState #Unconstitutional","sentiment":"Neutral"},{"line":"Damn you still hella quiet @BarackObama","sentiment":"Positive"},{"line":"You should catch an @Cardinals game @BarackObama. They have the best fans and the most abusive suburban cops.","sentiment":"Positive"},{"line":"@BarackObama is retweets not endorsements","sentiment":"Neutral"},{"line":"@SheilaBabsss @ANGLAESPNO @olazogilmay_ @JoshuaMacaspac_ @mathswifts @DaleSwifty @itsKimMcGraw @noynoyaquino @BarackObama or a dog","sentiment":"Neutral"},{"line":"\"@TimesofIsrael: Izzy Lemberg's Blog: @BarackObamaâ€™s ambush of #Israel http://t.co/bnKS2PwQpa via @timesofisrael\"","sentiment":"Positive"},{"line":"#Ferguson? \"@BarackObama: \"The death of Michael Brown is heartbreaking.\" â€”President Obama. Read the full statement: http://t.co/18DeLqMeVn\"","sentiment":"Neutral"},{"line":"Whats number one ? @BarackObama Answer Choice B: Public Safety. Good job. Let them clown on you, keep working. Yes sir.","sentiment":"Neutral"},{"line":"@SheilaBabsss @ANGLAESPNO @olazogilmay_ @JoshuaMacaspac_ @mathswifts @DaleSwifty @itsKimMcGraw @noynoyaquino @BarackObama or a cat","sentiment":"Neutral"},{"line":"Police firing tear gas at protesters?\n\n@BarackObama, WHAT KIND OF SHIT HOLE ARE YOU RUNNING HERE?! #Ferguson","sentiment":"Negative"},{"line":"barackobama: sirlightbulb: dear god I hope that no one on this website ever tries to run for president... http://t.co/a5G8AfX8bA","sentiment":"Neutral"},{"line":"@BarackObama @GovJayNixon what's happening in #Ferguson tonight is disgraceful.  The world is watching this.  #PoliceState #Unconstitutional","sentiment":"Neutral"},{"line":"Damn you still hella quiet @BarackObama","sentiment":"Positive"},{"line":"You should catch an @Cardinals game @BarackObama. They have the best fans and the most abusive suburban cops.","sentiment":"Positive"},{"line":"@BarackObama is retweets not endorsements","sentiment":"Neutral"}]
+
+function clickIdol()
+{
+
+}
+
+//var results = [{"line":"Police firing tear gas at protesters?\n\n@BarackObama, WHAT KIND OF SHIT HOLE ARE YOU RUNNING HERE?! #Ferguson","sentiment":"Negative"},{"line":"barackobama: sirlightbulb: dear god I hope that no one on this website ever tries to run for president... http://t.co/a5G8AfX8bA","sentiment":"Neutral"},{"line":"@BarackObama @GovJayNixon what's happening in #Ferguson tonight is disgraceful.  The world is watching this.  #PoliceState #Unconstitutional","sentiment":"Neutral"},{"line":"Damn you still hella quiet @BarackObama","sentiment":"Positive"},{"line":"You should catch an @Cardinals game @BarackObama. They have the best fans and the most abusive suburban cops.","sentiment":"Positive"},{"line":"@BarackObama is retweets not endorsements","sentiment":"Neutral"},{"line":"@SheilaBabsss @ANGLAESPNO @olazogilmay_ @JoshuaMacaspac_ @mathswifts @DaleSwifty @itsKimMcGraw @noynoyaquino @BarackObama or a dog","sentiment":"Neutral"},{"line":"\"@TimesofIsrael: Izzy Lemberg's Blog: @BarackObamaâ€™s ambush of #Israel http://t.co/bnKS2PwQpa via @timesofisrael\"","sentiment":"Positive"},{"line":"#Ferguson? \"@BarackObama: \"The death of Michael Brown is heartbreaking.\" â€”President Obama. Read the full statement: http://t.co/18DeLqMeVn\"","sentiment":"Neutral"},{"line":"Whats number one ? @BarackObama Answer Choice B: Public Safety. Good job. Let them clown on you, keep working. Yes sir.","sentiment":"Neutral"},{"line":"@SheilaBabsss @ANGLAESPNO @olazogilmay_ @JoshuaMacaspac_ @mathswifts @DaleSwifty @itsKimMcGraw @noynoyaquino @BarackObama or a cat","sentiment":"Neutral"},{"line":"Police firing tear gas at protesters?\n\n@BarackObama, WHAT KIND OF SHIT HOLE ARE YOU RUNNING HERE?! #Ferguson","sentiment":"Negative"},{"line":"barackobama: sirlightbulb: dear god I hope that no one on this website ever tries to run for president... http://t.co/a5G8AfX8bA","sentiment":"Neutral"},{"line":"@BarackObama @GovJayNixon what's happening in #Ferguson tonight is disgraceful.  The world is watching this.  #PoliceState #Unconstitutional","sentiment":"Neutral"},{"line":"Damn you still hella quiet @BarackObama","sentiment":"Positive"},{"line":"You should catch an @Cardinals game @BarackObama. They have the best fans and the most abusive suburban cops.","sentiment":"Positive"},{"line":"@BarackObama is retweets not endorsements","sentiment":"Neutral"}]
 function renderResults(){
     console.log("results:", results);
 
