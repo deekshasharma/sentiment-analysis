b6f55b64b723a46a149f5203c7b68383a280b46d
Deeksha Sharma <deeksha.sharma25@gmail.com>
2014-08-24 17:06:05 -0700

    Working on Websocket endpoint

--- restservice/src/main/java/WebSocket.java
+++ restservice/src/main/java/WebSocket.java
@@ -1,9 +1,35 @@
+import com.sentiments.twitter.TwitterStreaming;
+
+import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/tweets")
 public class WebSocket
 {
 
+    @OnOpen
+    private void openConnection(Session session)
+    {
+        System.out.println("Connected to new Session"+session.getId());
+    }
 
+    @OnMessage
+    private void getStreamOfTweets()
+    {
+        TwitterStreaming  twitterStreaming = TwitterStreaming.getInstance();
+
+    }
+
+    @OnError
+    private void logError()
+    {
+        System.out.println("Error in WebSocket API");
+    }
+
+    @OnClose
+    private void closeConnection(Session session, CloseReason closeReason)
+    {
+        System.out.println("Session with id "+ session.getId()+" is now closing due to "+ closeReason);
+    }
 
 }
